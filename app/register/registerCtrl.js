d535fc8f18d3709f73c9ab353c4f1dae5caad96f
kalyan5 <its.kalyan5@gmail.com>
2016-11-19 14:26:51 +0530

    can see now

---
+++ app/register/registerCtrl.js
@@ -0,0 +1,51 @@
+(function () {
+    function registerCtrl($scope, lookupSvc) {
+        var states = [{
+            "name": "Telangana",
+            code: "TS",
+            countryCode: "IN"
+}, {
+            "name": "Andhra Pradesh",
+            code: "AP",
+            countryCode: "IN"
+}, {
+            "name": "NewYork",
+            code: "NY",
+            countryCode: "US"
+}, {
+            "name": "Texas",
+            code: "TX",
+            countryCode: "US"
+}];
+        $scope.user = {};
+        $scope.register = function () {
+            console.log($scope.user);
+        };
+       // $scope.countries = lookupSvc.getCountries();
+
+        lookupSvc.getCountriesFromApi()
+        .then(function(response){
+            //console.log(response);
+            $scope.countries = response.data.countries;
+        })
+        .catch(function(errorResponse){
+            console.log(errorResponse);
+        });
+        
+        $scope.loadStatesByCountry = function () {
+            var countryCode = $scope.user.country.code;
+            $scope.states = getStateByCountry(countryCode);
+        };
+
+
+
+        function getStateByCountry(code) {
+            var stateList = _.where(states, {
+                countryCode: code
+            });
+            return stateList;
+        }
+    }
+    angular.module("register").controller("registerCtrl", registerCtrl);
+
+})();
